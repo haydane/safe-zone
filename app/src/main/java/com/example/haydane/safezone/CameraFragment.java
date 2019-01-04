f102ab9a490a38bef0d27d45c04d41f396222f0e
danehay <haydane9@gmail.com>
2019-01-05 01:33:07 +0700

    capture and save image in local storage

--- app/src/main/java/com/example/haydane/safezone/CameraFragment.java
+++ app/src/main/java/com/example/haydane/safezone/CameraFragment.java
@@ -1,7 +1,10 @@
 package com.example.haydane.safezone;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,16 +19,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import com.camerakit.CameraKitView;
 
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Timestamp;
 
 import static android.support.constraint.Constraints.TAG;
 
-public class CameraFragment extends Fragment implements CameraKitView.GestureListener {
+public class CameraFragment extends Fragment implements CameraKitView.GestureListener, View.OnClickListener {
     protected CameraKitView cameraKitView;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Context context;
+    MainActivity activity = (MainActivity)context;
+    Button btnCaputer;
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     @Nullable
     @Override
@@ -33,6 +42,11 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
         cameraKitView = view.findViewById(R.id.camera);
         cameraKitView.setGestureListener(this);
+        cameraKitView.requestPermissions(activity);
+        cameraKitView.setAspectRatio(2.2f);
+//        cameraKitView.setImageMegaPixels(2f);
+        btnCaputer = view.findViewById(R.id.btnCapture);
+        btnCaputer.setOnClickListener(this);
         return view;
     }
 
@@ -70,10 +84,7 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
     @Override
     public void onTap(CameraKitView cameraKitView, float v, float v1) {
         Log.d(TAG, "onTap v=: " + v + "  v1= " + v1);
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
+
 
     }
 
@@ -92,4 +103,37 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
 
     }
 
+
+    int TIME = 1;
+    @Override
+    public void onClick(final View v) {
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+
+                // If you need bitmap you could do this way as well xD  so can i test again? okay...hhahaha  I thought we could use this captured byte arry
+                Bitmap bitmap = BitmapFactory.decodeByteArray(capturedImage,0,capturedImage.length);
+
+                // Any problems ?
+                File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo_" + System.currentTimeMillis() + ".jpg");
+                try {
+                    FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                    outputStream.write(capturedImage);
+                    outputStream.close();
+                    Log.d(TAG, "onImage: "  + timestamp.getSeconds());
+                    Toast.makeText(v.getContext(),"Capture " + TIME, Toast.LENGTH_SHORT).show();
+                    TIME = TIME+1;
+
+                    if(TIME==2)
+                    {
+                        TIME = 1;
+                    }
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+    }
 }
