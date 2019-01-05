c96441471d1c74cbfb3c0d09b49a81ffddbd3e37
danehay <haydane9@gmail.com>
2019-01-05 13:53:10 +0700

    counter function

--- app/src/main/java/com/example/haydane/safezone/CameraFragment.java
+++ app/src/main/java/com/example/haydane/safezone/CameraFragment.java
@@ -1,14 +1,10 @@
 package com.example.haydane.safezone;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,7 +21,6 @@ import com.camerakit.CameraKitView;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.sql.Timestamp;
 
 import static android.support.constraint.Constraints.TAG;
 
@@ -33,8 +28,7 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
     protected CameraKitView cameraKitView;
     Context context;
     MainActivity activity = (MainActivity)context;
-    Button btnCaputer;
-    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    Button button;
 
     @Nullable
     @Override
@@ -44,9 +38,8 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
         cameraKitView.setGestureListener(this);
         cameraKitView.requestPermissions(activity);
         cameraKitView.setAspectRatio(2.2f);
-//        cameraKitView.setImageMegaPixels(2f);
-        btnCaputer = view.findViewById(R.id.btnCapture);
-        btnCaputer.setOnClickListener(this);
+        button = view.findViewById(R.id.btnCapture);
+        button.setOnClickListener(this);
         return view;
     }
 
@@ -103,31 +96,38 @@ public class CameraFragment extends Fragment implements CameraKitView.GestureLis
 
     }
 
+    boolean flag = true;
+    int TIME = 0;
+    public int counter()
+    {
+        if(flag == true) {
+            TIME = 1;
+            flag = false;
+        }
+        else
+        {
+            flag = true;
+            TIME= 2;
+        }
+        return TIME;
 
-    int TIME = 1;
+    }
+
+
     @Override
     public void onClick(final View v) {
         cameraKitView.captureImage(new CameraKitView.ImageCallback() {
             @Override
             public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
 
-                // If you need bitmap you could do this way as well xD  so can i test again? okay...hhahaha  I thought we could use this captured byte arry
                 Bitmap bitmap = BitmapFactory.decodeByteArray(capturedImage,0,capturedImage.length);
 
-                // Any problems ?
                 File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo_" + System.currentTimeMillis() + ".jpg");
                 try {
                     FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
                     outputStream.write(capturedImage);
                     outputStream.close();
-                    Log.d(TAG, "onImage: "  + timestamp.getSeconds());
-                    Toast.makeText(v.getContext(),"Capture " + TIME, Toast.LENGTH_SHORT).show();
-                    TIME = TIME+1;
-
-                    if(TIME==2)
-                    {
-                        TIME = 1;
-                    }
+                    Toast.makeText(v.getContext(),"Capture " + counter(), Toast.LENGTH_SHORT).show();
                 } catch (java.io.IOException e) {
                     e.printStackTrace();
                 }
